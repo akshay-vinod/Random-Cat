6eac9d07f1d5e9accd499fc3f8a315339be7da63
akshay-vinod <akshayvinod468@gmail.com>
2021-10-27 18:06:19 +0530

    ui added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="border">
+          <img
+            src="https://www.prestigeanimalhospital.com/sites/default/files/interesting-cat-facts.jpg"
+            className="object"
+            alt="cat"
+          />
+        </div>
+        <button class="button-57" role="button">
+          <span class="text">Click</span>
+          <span>Random Cat</span>
+        </button>
       </header>
     </div>
   );
