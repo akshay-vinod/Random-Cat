7df560c8847743b7b29006f3a83b8b834eab790d
akshay-vinod <akshayvinod468@gmail.com>
2021-10-27 21:54:31 +0530

    site is now fuctional

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [loader, setLoader] = useState(false);
   const [url, setUrl] = useState(
     "https://www.prestigeanimalhospital.com/sites/default/files/interesting-cat-facts.jpg"
   );
-  const makeRequest = () => {
-    console.log("hai");
+  const makeRequest = async () => {
+    fetch("https://thatcopy.pw/catapi/rest/")
+      .then((response) => response.json())
+      .then((data) => {
+        setLoader(true);
+        setTimeout(() => {
+          setLoader(false);
+          setUrl(data.url);
+        }, 1000);
+      });
   };
   return (
     <div className="App">
       <header className="App-header">
         <div className="border">
-          <img src={url} className="object" alt="cat" />
+          {loader ? (
+            <div className="spinner-3"></div>
+          ) : (
+            <img src={url} className="object" alt="cat" />
+          )}
         </div>
         <button className="button-57" role="button" onClick={makeRequest}>
           <span className="text">Click</span>
